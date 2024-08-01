b295c9963c4d53a53f69a6975824398d6081e03f
Thomas Brakutt <thomas.brakutt@gmail.com>
2024-08-01 21:43:19 +0200

    create file for exercice 02

---
+++ 02.Coding-Task-Use-operators-and-data types/script.js
@@ -0,0 +1,35 @@
+// 01) Write a String that logs out your first name
+console.log("T" + "h" + "o" + "m" + "a" + "s");
+
+// 02) Spot the problems & solve them
+
+// Error! Make it work
+// console.log("Hallo";
+console.log("Hallo");
+
+// Error! Make it work
+// console.log("That doesn"t work");
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+// console.log(50 + 2 > 52);
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+// console.log("333" == 333);
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+// console.log(600 + 510 / 2);
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+// console.log("2" + 2);
+console.log(2 + 2);
+
+// 03) Compare two conditions
+/*Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5 */
+
+console.log(25 / 5 === 50 / 10);
