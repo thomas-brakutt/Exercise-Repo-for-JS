da9bdec76cc6dde73a33241045a9925621cc94b7
Thomas Brakutt <thomas.brakutt@gmail.com>
2024-08-21 21:36:58 +0200

    edit Exercice

---
+++ 06.Coding-Task-using-loops/script-ex2.js
@@ -0,0 +1,20 @@
+/* 02) charCount function
+
+Implement a charCount function.
+
+ - The function has a first parameter for a word which is a string.
+ - The function has a second parameter which contains the character to search for which is a string.
+ - Check if the second parameter consists of only one character, if not, quit the function.
+ - The function returns the number of times the character from the second parameter is present in the word.
+ - The function should be case insensitive.
+
+// TODO: Implement the charCount function
+
+console.log(charCount("hello", "l"));
+// result should be: 2
+
+console.log(charCount("mama", "m"));
+// result should be: 2
+
+console.log(charCount("Resümee", "e"));
+// result should be: 3  */
